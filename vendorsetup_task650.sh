8a10a8813e63e22009b468158fa7d2431a8e0642
task650 <task650@gmail.com>
2014-04-05 12:40:18 -0700

    Unify d2 devices

--- vendorsetup_task650.sh
+++ vendorsetup_task650.sh
@@ -1,6 +1,5 @@
-add_lunch_combo aokp_jflteatt-userdebug
-add_lunch_combo aokp_jfltetmo-userdebug
-add_lunch_combo aokp_d2att-userdebug
+add_lunch_combo aokp_jflte-userdebug
+add_lunch_combo aokp_d2lte-userdebug
 add_lunch_combo aokp_manta-userdebug
 
 
